3e274fff6ad2ed80518f81275c43c35d02e0803e
sean lyons <sean@hexault.com>
2014-01-08 04:26:27 -0600

    progress - working. adding s# values to track highest movement sum in the past 2 seconds; added sad trombone for game over. settled on ajax + time desync estimates over websockets because joe convinced me websockets are stupid.

--- config.php
+++ config.php
@@ -1,49 +1,9 @@
 <?PHP
-include_once 'config.php';
+// define('TRAITOR', 1);
 
-//This is an API call that allows the client to synchronize time.
-//Once time is synched, this allows the server to dictate options and slowtime.
 
-$config = getConfig();
-
-if (empty($config)) {
-	$error = 'Config is empty.';
-	error_log($error);
-	echo $error;
-	return;
-}
-
-$url = "http://www.wordsmith.org/anagram/anagram.cgi?anagram=". $arg ."&t=1000&a=n";
-
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_HEADER, TRUE);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-$head = curl_exec($ch);
-curl_close($ch);
-
-go( $head );
-
-function go ( $input ) {
-	$begin = 'found. Displaying all:';
-	$end = 'bottomlinks';
-	
-	$begin = strlen($begin) + strpos($input, $begin);
-	$end = strpos($input, $end);
-
-	$words = substr($input, $begin, $end - $begin);
-	
-	$words = str_replace(array("\n", '<br>','</b>', '<'), array(' '), $words);
-	
-	$words = explode(' ', $words);
-	foreach ($words as $i => $w) {
-		if (strlen($w) >= 3) {
-			$sufficient[] = $w;
-		}
-	}
-	$sufficient = json_encode(array_unique($sufficient));
-error_log('sufficient = ' . $sufficient);
-	header('Content-type: application/json');
-	echo $sufficient;
-}
-?>
+return array(
+	'players' => 3,
+	'mode' => 'ffa', //enum: 
+	'traitor' => FALSE
+);
