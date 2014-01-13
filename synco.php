63b1a5c551db82ba158d983262351e7aa9756efb
sean lyons <sean@hexault.com>
2014-01-12 21:48:12 -0600

    fooled around with average movement values, which i'm abandoning for the time being; added support for long polling

--- synco.php
+++ synco.php
@@ -1,49 +1,54 @@
 <?PHP
-include_once 'config.php';
 
-//This is an API call that allows the client to synchronize time.
-//Once time is synched, this allows the server to dictate options and slowtime.
+$synco = new Synco();
+$response = $synco->init();
+echo $response;
+return;
 
-$config = getConfig();
-
-if (empty($config)) {
-	$error = 'Config is empty.';
-	error_log($error);
-	echo $error;
-	return;
-}
-
-$url = "http://www.wordsmith.org/anagram/anagram.cgi?anagram=". $arg ."&t=1000&a=n";
-
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_HEADER, TRUE);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-$head = curl_exec($ch);
-curl_close($ch);
-
-go( $head );
-
-function go ( $input ) {
-	$begin = 'found. Displaying all:';
-	$end = 'bottomlinks';
+class Synco {
+	public $args = array();
+	public $verb = '';
 	
-	$begin = strlen($begin) + strpos($input, $begin);
-	$end = strpos($input, $end);
-
-	$words = substr($input, $begin, $end - $begin);
-	
-	$words = str_replace(array("\n", '<br>','</b>', '<'), array(' '), $words);
-	
-	$words = explode(' ', $words);
-	foreach ($words as $i => $w) {
-		if (strlen($w) >= 3) {
-			$sufficient[] = $w;
-		}
+	function __construct() {
+		$this->args = $this->parseArgs( $_GET );	
 	}
-	$sufficient = json_encode(array_unique($sufficient));
-error_log('sufficient = ' . $sufficient);
-	header('Content-type: application/json');
-	echo $sufficient;
+
+	function init() {
+		switch($this->args['action']) {
+			default:
+				throw new Exception('No action undertaken.');
+			case 'ts':
+				$ts = $this->args['ts'];
+				$php_ts = $_SERVER["REQUEST_TIME_FLOAT"] * 1000;
+				//Divide by 1000 because microtime = 1e-6 and js Time() = 1e-3.
+				$diff = abs($ts - $php_ts) . 'microseconds';
+				return $diff;
+		}
+        
+        /************************************************************/
+        
+        $db = new Db();
+        $query = $db->generateQuery('select', 'long_poll_command_queue', NULL, array('client_sid' => $_SESSION['user_sid']));
+        $results = $db->performQuery( $query );
+        
+        
+        
+        /************************************************************/
+	
+	
+		$response = array('beep' => 'boop');
+		$response = $this->args;
+		return json_encode($response);
+	}
+
+
+	function parseArgs( $args ) {
+		if (!is_array($args) || empty($args)) {
+			throw new Exception('Invalid args passed to synco.');
+		}
+		foreach ($args as $k => $v) {
+			$new_args[htmlspecialchars($k)] = htmlspecialchars($v);
+		}
+		return $new_args;
+	}
 }
-?>
